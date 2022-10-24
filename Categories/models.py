effb42363b1cfd1a1dd808011f5b7583e44e6e4e
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-10-24 12:49:40 -0400

    Creating Model for ProdcutDetail

---
+++ Categories/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    Choices=[
+        ('Man','Man'),
+        ('Women','Women'),
+        ('Electronic','Electronic'),
+        ('Tshirt', 'Tshirt'),
+        ('Pants', 'Pants'),
+        ('Accessories', 'Accessories'),
+        ('Bags', 'Bags'),
+    ]
+
+    ParentCategory=models.CharField(choices=Choices,max_length=50,null=True,blank=True)
+    Parentitem=models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
+
+    def __str__(self):
+        fullpath=[self.ParentCategory]
+        parent=self.Parentitem
+        while parent is not None:
+            fullpath.append(parent.ParentCategory)
+            parent=parent.Parentitem
+        return '->'.join(fullpath[::-1])
