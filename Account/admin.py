f20823c0d452ef13bb0356fc5ebad888655ef41a
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-10-23 23:32:42 -0400

    RegistrationDone

--- Account/admin.py
+++ Account/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account,UserProfile
 admin.site.register(Account)
+admin.site.register(UserProfile)
 # Register your models here.
