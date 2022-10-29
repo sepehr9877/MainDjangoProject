de1d380046d670e81b3600b8ef10d88ccf583c3a
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-10-29 13:02:58 -0400

    Ajax Calling issue

--- Product/urls.py
+++ Product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import searchview,CategorySearch,ProductDetailView
+from .views import searchview,CategorySearch,ProductDetailView,Filtering,filter
 urlpatterns=[
     path('Search/',searchview.as_view(),name="Search"),
     path('Category/<slug:CategoryName>',CategorySearch.as_view(),name="CategorySearch"),
-    path('ProductDetail/<int:ID>',ProductDetailView.as_view(),name="ProductDetailView")
+    path('ProductDetail/<int:ID>',ProductDetailView.as_view(),name="ProductDetailView"),
+    path("Filtering/",Filtering.as_view()),
+    path("filtering/<color>",filter,name="filtering")
 ]
