689300e27be303922b8a9f83c637d6584cfce472
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-10-31 21:25:29 -0400

    finalize order function

--- Order/models.py
+++ Order/models.py
@@ -6,6 +6,12 @@ class Order(models.Model):
     UserOder=models.ForeignKey(UserProfile,on_delete=models.CASCADE)
     PriceOrder=models.IntegerField(null=True,blank=True)
     OrderDate=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.UserOder.username
 class OrderDetail(models.Model):
     orderdetail=models.ForeignKey(Order,on_delete=models.CASCADE)
     productorder=models.ForeignKey(ProductDetail,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.orderdetail.UserOder.username +"____"+self.productorder.Pro_Detail.title+"_____"+self.productorder.Pro_color.Color_Rate
--- Order/views.py
+++ Order/views.py
@@ -8,12 +8,12 @@ from django.views.generic import CreateView
 def Creating(request):
     print(request.GET.get('selected_color'))
 class CreatingOrder(CreateView):
-    model = Order,OrderDetail,ProductDetail
-    OrderdetailId=None
+    Orderdetail=None
     def get(self, request, *args, **kwargs):
         color=self.request.GET.get("color_value")
         size=self.request.GET.get("size_value")
         productid=self.request.GET.get("productid")
+        print(productid,size,color)
         Order_User=self.create_order()
         Selected_Prodcut=self.check_productdetail(productid=productid,color=color,size=size)
         self.CreateOrderDetail(Product_Detail=Selected_Prodcut,OrderSpec=Order_User)
@@ -24,24 +24,26 @@ class CreatingOrder(CreateView):
         return JsonResponse(data,safe=False)
     def create_order(self):
         userid=self.request.user.id
-        user_order=Order.objects.get(UserOder_id=userid)
+        user_order=Order.objects.filter(UserOder_id=3)
+        print(user_order)
         if len(user_order)==0:
-            user_order=Order.objects.create(UserOder_id=userid)
+            user_order=Order.objects.create(UserOder_id=3)
 
         return user_order
     def check_productdetail(self,productid,color,size):
-        selected_product=ProductDetail.objects.get(Pro_Detail_id=productid,
+        selected_product=ProductDetail.objects.filter(Pro_Detail_id=productid,
                                                    Pro_color__Color_Rate=color,
                                                    Pro_size__SizeRate=size)
         return selected_product
     def CreateOrderDetail(self,Product_Detail,OrderSpec):
-        OrderDetailEl=OrderDetail.objects.get(orderdetail_id=Product_Detail.id,productorder_id=OrderSpec.id)
+        print(Product_Detail,OrderSpec)
+        OrderDetailEl=OrderDetail.objects.filter(orderdetail__in=OrderSpec,productorder__in=Product_Detail)
         if len(OrderDetailEl)==0:
-            OrderDetailEl=OrderDetail.objects.create(orderdetail_id=Product_Detail.id, productorder_id=OrderSpec.id)
-        self.OrderdetailId = OrderDetailEl.id
+            OrderDetailEl=OrderDetail.objects.create(orderdetail=OrderSpec[0], productorder=Product_Detail[0])
+        self.Orderdetail = OrderDetailEl
         return OrderDetailEl
     def CheckOrderDetail(self):
-        CheckOrder=OrderDetail.objects.get(orderdetail_id=self.OrderdetailId)
+        CheckOrder=OrderDetail.objects.filter(id=self.Orderdetail[0].id)
         if len(CheckOrder)==0:
             return False
         else:
