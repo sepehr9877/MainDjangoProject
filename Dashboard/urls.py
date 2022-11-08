7232f4e4e9993a96ef356a80a02009d197539687
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-11-08 12:48:35 -0500

    Transaction Records

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashBoardPage,ProfileEditPage
+from .views import DashBoardPage,ProfileEditPage,MyTransactionOrder
 urlpatterns=[
     path('DashboardPage',DashBoardPage.as_view()),
-    path('EditProfile',ProfileEditPage.as_view())
+    path('EditProfile',ProfileEditPage.as_view()),
+    path('MyTransactionOrder',MyTransactionOrder.as_view())
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView,View,CreateView
 from Order.models import Order,OrderDetail,ShippingDetail,CardSpecification
 from Order.forms import ShippingForm
@@ -12,7 +13,9 @@ class DashBoardPage(DetailView):
     def get_object(self, queryset=None):
         userid=self.request.user.id
         self.Selected_Order_user=Order.objects.filter(UserOder__user_id=userid,transaction=True).order_by('OrderDate').first()
-        queryset=OrderDetail.objects.filter(orderdetail__UserOder__user_id=self.Selected_Order_user.id).all()
+        print("Order")
+        print(self.Selected_Order_user)
+        queryset=OrderDetail.objects.filter(orderdetail_id=self.Selected_Order_user.id).all()
         return queryset
     def get_context_data(self,*args,**kwargs):
         context=super(DashBoardPage, self).get_context_data(*args,**kwargs)
@@ -32,12 +35,35 @@ class ProfileEditPage(DetailView):
             "lastname":self.request.user.last_name,
             "phone":account_detail.phonenumber,
             "email":self.request.user.email
-
-
         }
         context={"Profile_Spec":UpdateProfile(initial=initial_date)}
         return render(self.request,template_name=self.template_name,context=context)
-    # def get_context_data(self, *args,**kwargs):
-    #     context=super(ProfileEditPage, self).get_context_data(*args,**kwargs)
-    #     context['EditProfile']=UpdateProfile()
-    #     return context
+    def post(self, request, *args, **kwargs):
+        profile_form=UpdateProfile(data=self.request.POST or None)
+        userid=self.request.user.id
+        if profile_form.is_valid():
+            username=profile_form.cleaned_data['username']
+            lastname=profile_form.cleaned_data['lastname']
+            phone=profile_form.cleaned_data['phone']
+            email=profile_form.cleaned_data['email']
+            user=User.objects.filter(id=userid).first()
+            user.first_name=username
+            user.last_name=lastname
+            user.email=email
+            user.save()
+            account=Account.objects.filter(user_id=userid).update(phonenumber=phone)
+        return redirect('/EditProfile')
+class MyTransactionOrder(DetailView):
+    template_name = 'dashboard/TransactionPage.html'
+    all_My_Order=None
+    def get_object(self, queryset=None):
+        userid=self.request.user.id
+        selected_account=Account.objects.filter(user_id=userid).first()
+        Order_user=Order.objects.filter(UserOder__user_id=userid).all()
+        shippingdetail=ShippingDetail.objects.filter(ShipOrder__UserOder_id=selected_account.id)
+        print(shippingdetail)
+        QuerySet=[]
+        for item in Order_user:
+            QuerySet.append(OrderDetail.objects.filter(orderdetail_id=item.id))
+        queryset=QuerySet
+        return queryset
